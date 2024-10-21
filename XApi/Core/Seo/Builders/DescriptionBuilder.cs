3538dd5962ffbf0529dedd603e4f0a00d462fea0
spyr0h <jean.pougetoux@outlook.fr>
2024-10-22 01:05:54 +0200

    fix seo metadata

--- XApi/Core/Seo/Builders/DescriptionBuilder.cs
+++ XApi/Core/Seo/Builders/DescriptionBuilder.cs
@@ -8,11 +8,13 @@ public class DescriptionBuilder : IDescriptionBuilder
 {
     public string BuildFrom(SearchCriteria criteria)
     {
+        var catRepresentation = string.Join(' ', criteria.Categories.Select(t => t.Value) ?? []).CapitalizeFirstLetter();
+        catRepresentation = string.IsNullOrEmpty(catRepresentation) ? string.Empty : $"{catRepresentation} ";
         var tagRepresentation = string.Join(' ', criteria.Tags.Select(t => t.Value) ?? []).CapitalizeFirstLetter();
         tagRepresentation = string.IsNullOrEmpty(tagRepresentation) ? string.Empty : $"{tagRepresentation} ";
         var pornstarRepresentation = string.Join(", ", criteria.Pornstars.Select(p => p.Value?.CapitalizeFirstLetterOfEachWord() ?? "") ?? []);
         pornstarRepresentation = string.IsNullOrEmpty(pornstarRepresentation) ? string.Empty : $" of {pornstarRepresentation}";
 
-        return $"{tagRepresentation}Porn Videos{pornstarRepresentation}";
+        return $"{catRepresentation}{tagRepresentation}Porn Videos{pornstarRepresentation}";
     }
 }
